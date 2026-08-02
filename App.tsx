b193b6b97ea1bbf93bc79deddbcb68633b35a1f9
Mitch Aldrich <mitch.w.aldrich@gmail.com>
2026-08-02 18:17:15 -0400

    set up dev environment and customize home screen

--- App.tsx
+++ App.tsx
@@ -1,44 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  Alert,
+  Pressable,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const experimentButton = () => {
+    Alert.alert('Coming Soon');
+  };
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.title}>Native Audio Lab</Text>
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+          <Text style={styles.subtitle}>
+            A playground for audio, pitch detection, and mobile engineering.
+          </Text>
+          {/* <View style={styles.button}> */}
+          <Pressable onPress={experimentButton} style={styles.button}>
+            <Text style={styles.buttonText}>Let the experiment begin!</Text>
+          </Pressable>
+          {/* </View> */}
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#215D6E',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 24,
+  },
+  title: {
+    color: 'white',
+    fontSize: 34,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  subtitle: {
+    color: '#9CA3AF',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 16,
+    lineHeight: 28,
+  },
+  button: {
+    minHeight: 52,
+    paddingHorizontal: 20,
+    borderRadius: 14,
+
+    backgroundColor: '#6e3221',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    shadowColor: '#000',
+    shadowOpacity: 0.12,
+    shadowRadius: 10,
+    shadowOffset: { width: 0, height: 4 },
+
+    elevation: 3,
+    marginTop: 20,
+  },
+
+  buttonPressed: {
+    opacity: 0.9,
+    transform: [{ scale: 0.98 }],
+  },
+
+  buttonText: {
+    color: '#FFF',
+    fontSize: 16,
+    fontWeight: '600',
+    letterSpacing: 0.2,
   },
 });
 
